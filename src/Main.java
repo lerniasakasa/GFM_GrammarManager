168281813ef688306da80586aa7bb8b34d6a6566
Lernia Sakasa <sksler002@cs.uct.ac.za>
2023-08-15 02:06:26 +0200

    gfm_source

---
+++ src/Main.java
@@ -0,0 +1,15 @@
+import org.grammaticalframework.pgf.*;
+
+public class Main {
+    public static void main(String[] args) {
+
+        try{
+            System.out.println("running!");
+        }
+        catch(Exception e){
+
+            e.printStackTrace();
+        }
+
+    }
+}
